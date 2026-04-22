a1b833850d2a3e47d332e69a6ae1eb05b4dee238
JacksonPrine <jacksonprine1@gmail.com>
2026-04-22 17:14:28 -0500

    Update DRUM_MACHINE.cpp

--- DRUM_MACHINE.cpp
+++ DRUM_MACHINE.cpp
@@ -27,12 +27,23 @@ void Write_SR_LCD(uint8_t temp);
 void Write_Instr_LCD(uint8_t code);
 uint8_t Read_Keypad();
 void Init_Timer2(void);
+void Write_SR_7S(uint8_t temp_Enable, uint8_t temp_Digit);
+void Write_7Seg(uint8_t temp_Enable, uint8_t temp_Digit);
+void Delay(volatile uint32_t count);
+void Init_buzzer(void);
+void Play_Buzz_Kick(void);
+void Play_Buzz_Snare(void);
+void Play_Buzz_Hat(void);
+
+
 
 //defining bitfield masks for each drum sound - this allows us to play multiple drum sounds at a single step.
 #define KICK_BIT 0b1
 #define SNARE_BIT 0b10
 #define HIHAT_BIT 0b100
 #define OTHER_BIT 0b1000
+
+//Simple version: Let Kick = 1, Snare = 2, and Hihat = 3.
 
 //globally defining the sequence size as 16
 #define BEAT_SIZE 16
@@ -53,6 +64,13 @@ int main(void)
 		MX_GPIO_Init();
 		//Initializing timer 2 to interrupt at 120 bpm.
 		Init_Timer2();
+	
+		Init_buzzer();
+	
+		pattern[0] = 1;
+		pattern[4] = 2;
+		pattern[8] = 1;
+		pattern[12] = 2;
 
     while (1)
     {
@@ -67,20 +85,279 @@ int main(void)
 			if(LEDtemp == 0) {
 				//Turn on LED 3
 				GPIOC->ODR |= (1<<8);
+				if(step%4 == 0)		//1.000
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 0); Delay(1);
+					Write_7Seg(2, 0); Delay(1);
+					Write_SR_7S(0x08, 0x79); Delay(1);
+					
+					if(pattern[0] == 1)
+						Play_Buzz_Kick();
+					if(pattern[0] == 2)
+						Play_Buzz_Snare();
+					if(pattern[0] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
+				else if(step%4 == 1)//1.250
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 5); Delay(1);
+					Write_7Seg(2, 2); Delay(1);
+					Write_SR_7S(0x08, 0x79); Delay(1);
+					
+					if(pattern[1] == 1)
+						Play_Buzz_Kick();
+					if(pattern[1] == 2)
+						Play_Buzz_Snare();
+					if(pattern[1] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
+				else if(step%4 == 2)//1.500
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 0); Delay(1);
+					Write_7Seg(2, 5); Delay(1);
+					Write_SR_7S(0x08, 0x79); Delay(1);
+					
+					if(pattern[2] == 1)
+						Play_Buzz_Kick();
+					if(pattern[2] == 2)
+						Play_Buzz_Snare();
+					if(pattern[2] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
+				else if(step%4 == 3)//1.750
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 5); Delay(1);
+					Write_7Seg(2, 7); Delay(1);
+					Write_SR_7S(0x08, 0x79); Delay(1);
+					
+					if(pattern[3] == 1)
+						Play_Buzz_Kick();
+					if(pattern[3] == 2)
+						Play_Buzz_Snare();
+					if(pattern[3] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
 			}
 			else if (LEDtemp == 1) {
 				//Turn on LED 2
 				GPIOC->ODR |= (1<<7);
+				if(step%4 == 0)//2.000
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 0); Delay(1);
+					Write_7Seg(2, 0); Delay(1);
+					Write_SR_7S(0x08, 0x24); Delay(1);
+					
+					if(pattern[4] == 1)
+						Play_Buzz_Kick();
+					if(pattern[4] == 2)
+						Play_Buzz_Snare();
+					if(pattern[4] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
+				else if(step%4 == 1)//2.250
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 5); Delay(1);
+					Write_7Seg(2, 2); Delay(1);
+					Write_SR_7S(0x08, 0x24); Delay(1);
+					
+					if(pattern[5] == 1)
+						Play_Buzz_Kick();
+					if(pattern[5] == 2)
+						Play_Buzz_Snare();
+					if(pattern[5] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
+				else if(step%4 == 2)//2.500
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 0); Delay(1);
+					Write_7Seg(2, 5); Delay(1);
+					Write_SR_7S(0x08, 0x24); Delay(1);
+					
+					if(pattern[6] == 1)
+						Play_Buzz_Kick();
+					if(pattern[6] == 2)
+						Play_Buzz_Snare();
+					if(pattern[6] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
+				else if(step%4 == 3)//2.750
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 5); Delay(1);
+					Write_7Seg(2, 7); Delay(1);
+					Write_SR_7S(0x08, 0x24); Delay(1);
+					
+					if(pattern[7] == 1)
+						Play_Buzz_Kick();
+					if(pattern[7] == 2)
+						Play_Buzz_Snare();
+					if(pattern[7] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
 			}
 			else if (LEDtemp == 2) {
 				//Turn on LED 1
 				GPIOA->ODR |= (1<<0);
+				if(step%4 == 0)//3.000
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 0); Delay(1);
+					Write_7Seg(2, 0); Delay(1);
+					Write_SR_7S(0x08, 0x30); Delay(1);
+					
+					if(pattern[8] == 1)
+						Play_Buzz_Kick();
+					if(pattern[8] == 2)
+						Play_Buzz_Snare();
+					if(pattern[8] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
+				else if(step%4 == 1)//3.250
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 5); Delay(1);
+					Write_7Seg(2, 2); Delay(1);
+					Write_SR_7S(0x08, 0x30); Delay(1);
+					
+					if(pattern[9] == 1)
+						Play_Buzz_Kick();
+					if(pattern[9] == 2)
+						Play_Buzz_Snare();
+					if(pattern[9] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
+				else if(step%4 == 2)//3.500
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 0); Delay(1);
+					Write_7Seg(2, 5); Delay(1);
+					Write_SR_7S(0x08, 0x30); Delay(1);
+					
+					if(pattern[10] == 1)
+						Play_Buzz_Kick();
+					if(pattern[10] == 2)
+						Play_Buzz_Snare();
+					if(pattern[10] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
+				else if(step%4 == 3)//3.750
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 5); Delay(1);
+					Write_7Seg(2, 7); Delay(1);
+					Write_SR_7S(0x08, 0x30); Delay(1);
+					
+					if(pattern[11] == 1)
+						Play_Buzz_Kick();
+					if(pattern[11] == 2)
+						Play_Buzz_Snare();
+					if(pattern[11] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
 			}
 			else if (LEDtemp == 3) {
 				//Turn on LED 0
 				GPIOA->ODR |= (1<<1);
+				if(step%4 == 0)//4.000
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 0); Delay(1);
+					Write_7Seg(2, 0); Delay(1);
+					Write_SR_7S(0x08, 0x19); Delay(1);
+					
+					if(pattern[12] == 1)
+						Play_Buzz_Kick();
+					if(pattern[12] == 2)
+						Play_Buzz_Snare();
+					if(pattern[12] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
+				else if(step%4 == 1)//4.250
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 5); Delay(1);
+					Write_7Seg(2, 2); Delay(1);
+					Write_SR_7S(0x08, 0x19); Delay(1);
+					
+					if(pattern[13] == 1)
+						Play_Buzz_Kick();
+					if(pattern[13] == 2)
+						Play_Buzz_Snare();
+					if(pattern[13] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
+				else if(step%4 == 2)//4.500
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 0); Delay(1);
+					Write_7Seg(2, 5); Delay(1);
+					Write_SR_7S(0x08, 0x19); Delay(1);
+					
+					if(pattern[14] == 1)
+						Play_Buzz_Kick();
+					if(pattern[14] == 2)
+						Play_Buzz_Snare();
+					if(pattern[14] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
+				else if(step%4 == 3)//4.750
+				{
+					Write_7Seg(4, 0); Delay(1);
+					Write_7Seg(3, 5); Delay(1);
+					Write_7Seg(2, 7); Delay(1);
+					Write_SR_7S(0x08, 0x19); Delay(1);
+					
+					if(pattern[15] == 1)
+						Play_Buzz_Kick();
+					if(pattern[15] == 2)
+						Play_Buzz_Snare();
+					if(pattern[15] == 3) {
+						Play_Buzz_Hat();
+					}
+				}
 			}
     }
+}
+
+void Init_buzzer()
+{
+uint32_t temp;
+RCC->AHB2ENR |= RCC_AHB2ENR_GPIOCEN;		/* enable GPIOC clock */
+
+temp = GPIOC->MODER;
+temp &= ~(0x03<<(2*9)); 
+temp|=(0x01<<(2*9)); 
+GPIOC->MODER = temp;
+
+temp=GPIOC->OTYPER;
+temp &=~(0x01<<9); 
+GPIOC->OTYPER=temp;
+
+temp=GPIOC->PUPDR;
+temp&=~(0x03<<(2*9)); 
+GPIOC->PUPDR=temp;
 }
 
 
@@ -199,15 +476,15 @@ RCC->AHB2ENR |= RCC_AHB2ENR_GPIOAEN;
            GPIOA->PUPDR=temp;
 
       /* LCD controller reset sequence */
-      HAL_Delay(20);
+      Delay(20);
       LCD_nibble_write(0x30,0);
-      HAL_Delay(5);
+      Delay(5);
       LCD_nibble_write(0x30,0);
-      HAL_Delay(1);
+      Delay(1);
       LCD_nibble_write(0x30,0);
-      HAL_Delay(1);
+      Delay(1);
       LCD_nibble_write(0x20,0);
-      HAL_Delay(1);
+      Delay(1);
      
            
       Write_Instr_LCD(0x28); /* set 4 bit data LCD - two line display - 5x8 font*/
@@ -331,7 +608,7 @@ uint8_t Read_Keypad()
         return 255;
     }
 
-    HAL_Delay(25);
+    Delay(25);
 
     // ---- Scan columns ONCE ----
 
@@ -342,7 +619,7 @@ uint8_t Read_Keypad()
 
     // Column 0
     GPIOB->ODR |= (1<<1);
-    HAL_Delay(2);
+    Delay(2);
 
     if(GPIOB->IDR & (1<<8)) return 1;
     if(GPIOB->IDR & (1<<9)) return 4;
@@ -352,7 +629,7 @@ uint8_t Read_Keypad()
     // Column 1
     GPIOB->ODR &= ~(1<<1);
     GPIOB->ODR |= (1<<2);
-    HAL_Delay(2);
+    Delay(2);
 
     if(GPIOB->IDR & (1<<8)) return 2;
     if(GPIOB->IDR & (1<<9)) return 5;
@@ -362,7 +639,7 @@ uint8_t Read_Keypad()
     // Column 2
     GPIOB->ODR &= ~(1<<2);
     GPIOB->ODR |= (1<<3);
-    HAL_Delay(2);
+    Delay(2);
 
     if(GPIOB->IDR & (1<<8)) return 3;
     if(GPIOB->IDR & (1<<9)) return 6;
@@ -372,7 +649,7 @@ uint8_t Read_Keypad()
     // Column 3
     GPIOB->ODR &= ~(1<<3);
     GPIOB->ODR |= (1<<4);
-    HAL_Delay(2);
+    Delay(2);
 
     if(GPIOB->IDR & (1<<8)) return 10;
     if(GPIOB->IDR & (1<<9)) return 11;
@@ -395,7 +672,7 @@ void Init_Timer2() {
 	TIM2->PSC = 399; //4 MHz (system clock) / 399 + 1 = 10,000 Hz
 	
 	//Setting the auto-reload, which is how often the the timer2 restarts
-	TIM2->ARR = 1249; // 10,000/(1249+1) = 8 Hz.
+	TIM2->ARR = 2499; // 10,000/(1249+1) = 8 Hz.
 	
 	//This line immediately updates Timer 2's PRC and ARR, preventing anything weird happening during first timer cycle.
 	TIM2->EGR |= TIM_EGR_UG;
@@ -432,6 +709,54 @@ void TIM2_IRQHandler(void) {
 	}
 }
 
+void Write_SR_7S(uint8_t temp_Enable, uint8_t temp_Digit)
+{
+	int i;
+	uint8_t mask=0b10000000;
+	for(i=0; i<8; i++)
+	{
+		if((temp_Digit&mask)==0) 
+				GPIOB->ODR&=~(1<<5);
+		else
+			GPIOB->ODR|=(1<<5);
+		/*	Sclck */
+		GPIOA->ODR&=~(1<<5);
+		Delay(1);
+		GPIOA->ODR|=(1<<5);
+		Delay(1);
+		mask=mask>>1;
+	}
+	mask=0b10000000;
+	for(i=0; i<8; i++)
+	{
+		if((temp_Enable&mask)==0) 
+			GPIOB->ODR&=~(1<<5);
+		else
+			GPIOB->ODR|=(1<<5);
+		/*	Sclck */
+		GPIOA->ODR&=~(1<<5);
+		/*Delay(1);*/ 
+		GPIOA->ODR|=(1<<5);
+		/*Delay(1);	*/ 
+		mask=mask>>1;
+	}
+	/*Latch*/
+	GPIOC->ODR|=(1<<10); 
+	GPIOC->ODR&=~(1<<10);
+}
+
+
+void Write_7Seg(uint8_t temp_Enable, uint8_t temp_Digit)
+{
+uint8_t Enable[5] = {0x00, 0x08, 0x04, 0x02, 0x01};
+/* Enable[i] can enable display i by writing one to DIGIT i and zeros to the other Digits */
+
+	uint8_t Digit[10]= {0xC0, 0xF9, 0xA4, 0xB0, 0x99, 0x92, 0x82, 0xF8, 0x80, 0x90};
+
+Write_SR_7S(Enable[temp_Enable], Digit[temp_Digit]);
+}
+
+
 void Write_SR_LCD(uint8_t temp)
 {
 int i;
@@ -454,6 +779,35 @@ for(i=0; i<8; i++) {
     GPIOA->ODR|=(1<<10);
     GPIOA->ODR&=~(1<<10);
 }
+void Play_Buzz_Kick (void) {
+		for (int i = 0; i < 50; i++)
+		{
+		GPIOC->ODR|=(1<<9);
+		Delay(4);
+		GPIOC->ODR&=~(1<<9);
+		Delay(4);
+		}
+	}
+void Play_Buzz_Snare (void) {
+		for (int i = 0; i < 50; i++)
+		{
+		GPIOC->ODR|=(1<<9);
+		Delay(3);
+		GPIOC->ODR&=~(1<<9);
+		Delay(3);
+		}
+	}
+
+void Play_Buzz_Hat (void) {
+	for (int i = 0; i < 50; i++)
+	{
+	GPIOC->ODR|=(1<<9);
+	Delay(2);
+	GPIOC->ODR&=~(1<<9);
+	Delay(2);
+	}
+}
+
 
 /* ---------------------- CLOCK CONFIGURATION ---------------------- */
 void SystemClock_Config(void)
@@ -492,7 +846,13 @@ void Error_Handler(void)
   }
 }
 
-
+void Delay(volatile uint32_t count)
+{
+    while (count > 0)
+    {
+        count--;
+    }
+}
 
 // ! IMPORTANT ! All code below this line is generated by STM32CubeMX and should not be modified
 #ifdef  USE_FULL_ASSERT
